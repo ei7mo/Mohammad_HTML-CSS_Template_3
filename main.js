0ccb3ab549e73089496fcac81ed0b2835afe1466
Eric-Miles18 <ericmiles199@gmail.com>
2026-05-08 23:14:29 +0300

    Added a timer count down for the event section

--- main.js
+++ main.js
@@ -45,3 +45,24 @@ function startCount(num) {
     }
   }, 2000 / goal);
 }
+
+// Set count down of events
+let countDownDate = new Date("Dec 31, 2026 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+
+  let dateDiff = countDownDate - dateNow;
+
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+}, 1000);
